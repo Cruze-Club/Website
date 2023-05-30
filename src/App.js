a95ee506c0bdfc8e9043ae6d9dcd083299e13860
Rafay Abbas <rafayabbas10@gmail.com>
2023-05-31 03:44:18 +0500

    Website 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import logo from './././assets/logo.png'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="nav">
+        <div className="logo">
+          <img className="nav-logo" src={logo} alt="logo" />
+        </div>
+        <div className="nav-list">
+          <ol>
+            <li><a className="selected-page" href="/">home</a></li>
+            <li><a href="/">waitlist</a></li>
+            <li><a href="/">carpool</a></li>
+            <li><a href="/">about</a></li>
+          </ol>
+        </div>
+      </div>
+      <div className="body">
+        <div className="slogan">
+          <h1><span>Cruze</span>through</h1>
+        </div>
+      </div>
+    </>
   );
 }
 
