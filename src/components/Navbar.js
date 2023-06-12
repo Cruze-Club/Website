7201ead218c46d292310a7588999431cb98b4c2e
Rafay Abbas <rafayabbas10@gmail.com>
2023-06-12 09:26:01 +0500

    responsive 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,29 @@
 import React from 'react'
 import logo from '../assets/logo.png'
 import '../App.css';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
-               <div className="nav">
-        <div className="logo">
-            <img className="nav-logo" src={logo} alt="logo" />
+        <div className="navMenu">
+            <div className="nav">
+                <div className="logo">
+                    <img className="nav-logo" src={logo} alt="logo" />
+                </div>
+                
+                <div className="nav-list">
+                    <ol>
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to='/waitlist'>Waitlist
+                        </Link></li>
+                        <li><Link to='/Carpool'>Carpool
+                        </Link></li>
+                        <li><Link to='/about'>About
+                        </Link></li>
+                    </ol>
+                </div>
+            </div>
         </div>
-        <div className="nav-list">
-            <ol>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/waitlist'>Waitlist
-            </Link></li>
-                <li><Link to='/Carpool'>Carpool
-            </Link></li>
-                <li><Link to='/about'>About
-            </Link></li>
-            </ol>
-        </div>
-    </div>
     )
 }
 
